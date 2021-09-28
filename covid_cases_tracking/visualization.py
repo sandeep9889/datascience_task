b57e678c5c816393ffc4e7acefc572a9a8ba55b4
sandeep <sandeepchauhan9630228313@gmail.com>
2021-09-28 12:38:24 +0530

    covid_visualization

---
+++ covid_cases_tracking/visualization.py
@@ -0,0 +1,3 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+df =pd.read_csv('WHO COVID-19 global table data September 28th 2021 at 10.44.53 AM')
--- matplotlib/matlplottest.py
+++ matplotlib/matlplottest.py
@@ -1,5 +1,10 @@
+from matplotlib import colors
 import matplotlib.pyplot as plt
 month_number =[1,2,3,4,5,6,7]
 new_deaths = [213,2729,37718,184064,143119,136073,165003]
-plt.plot(month_number,new_deaths)
+plt.plot(month_number, new_deaths, color='red', label ='India')
+plt.ticklabel_format(axis='y',style="plain")
+plt.title("new reported deaths global")
+plt.xlabel("month number")
+plt.ylabel("no of deaths")
 plt.show()
