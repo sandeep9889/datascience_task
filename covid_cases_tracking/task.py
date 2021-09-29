142654ba50e492dafcb1844ad78cd08ff917b408
sandeep <sandeepchauhan9630228313@gmail.com>
2021-09-29 08:21:42 +0530

    france map

--- covid_cases_tracking/task.py
+++ covid_cases_tracking/task.py
@@ -12,6 +12,12 @@ who_time_series=DataFrame(df)
 # who_time_series = np.array(df1)
 # who_time_series = who_time_series[1:]
 # print(who_time_series)
+
 france = who_time_series[who_time_series["Country"]== "France"]
-print(france)
+france_june = who_time_series[who_time_series["Date_reported"] == france["Date_reported"[5:7]]]
+plt.plot(france_june,france["Cumulative_cases"],label ="france")
+plt.show()
+#2019-07-01
+
+# print(france)
 
