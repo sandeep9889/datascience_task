a78d4b5ef8de73b321c0f121e556e4d035ded5d2
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-05 15:13:20 +0530

    data frame

--- pandas/pandaas3.py
+++ pandas/pandaas3.py
@@ -1,6 +1,8 @@
 import pandas as pd
 data = {"a":[1,2,3], "b":[5,8,4]}
 df = pd.DataFrame(data)
+B=df.drop("a",axis=1,inplace=True)
+print(B)
 df["c"] = df["a"]+df["b"]
 print(df)
 print(df.iloc[2]["c"])
