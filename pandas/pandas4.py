86aa8cbf817b1520223259aa89cd73c349a3bb26
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-08 16:06:31 +0530

    index_and_colums

--- pandas/pandas4.py
+++ pandas/pandas4.py
@@ -2,5 +2,7 @@
 import pandas as pd
 data = {"weight":[50,75,55,60],"height":[178,190,175,150]}
 df = pd.DataFrame(data,index=["randy","rem","jhon","cena"])
-df.head()
+# df.head()
 print(df)
+san=df.groupby(data["weight".max()])
+print(san)
