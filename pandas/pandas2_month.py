dc8989683b15e6b892dc5a73081c5fefad1488e9
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-26 11:27:54 +0530

    matplotlib

--- pandas/pandas2_month.py
+++ pandas/pandas2_month.py
@@ -1,4 +1,4 @@
 import pandas as pd
 data = {"jan":[255,260,265,270,275],"feb":[280,285,290,295,300],"march":[355,360,365,370,375],"aprail":[380,385,390,395,400]}
 df = pd.DataFrame(data)
-print(df.jan)
+print(df.jan[0])
--- pandas/pandas4.py
+++ pandas/pandas4.py
@@ -4,5 +4,5 @@ data = {"weight":[50,75,55,60],"height":[178,190,175,150]}
 df = pd.DataFrame(data,index=["randy","rem","jhon","cena"])
 # df.head()
 print(df)
-san=df.groupby(data["weight".max()])
+san=df.groupby(data["weight"])
 print(san)
