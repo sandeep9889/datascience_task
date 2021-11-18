403168a1ce55dc425e104f6295c1c8eb402a7c64
sandeep <sandeepchauhan9630228313@gmail.com>
2021-11-18 20:42:13 +0530

    Kneighbors classifier

--- machine learning/classification/classification.py
+++ machine learning/classification/classification.py
@@ -12,7 +12,8 @@ print(features[0],label[0])
 
 # training the classifier 
 clf = KNeighborsClassifier()  
-clf.fit(features,label) #here we can fit means train the data
+clf.fit(features,label) #here we can fit means train the data --- train
 
 preds =clf.predict([[31,1,1,1]]) # the new set that we have to get label and this will in test condition predict the elemnt that has be input [[1,1,1,1]]
+                                  #------test 
 print(preds)
